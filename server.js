fc97b2c0ad694f8ef8c07edc0e0f665b3808efac
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2016-09-26 21:48:23 +0530

    pushing large set of angularjs examples

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+var jsonServer = require('json-server')
+
+var express = require("express");
+
+var server = jsonServer.create();
+ 
+var middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use('/examples', express.static('examples'));
+
+server.listen(3000, function (err) {
+    if (!err) {
+         console.log('Server is running  at 3000')
+    }
+})
