00ff593455f658360f260d9f0ce1be04b8b78a96
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2016-09-27 23:27:13 +0530

    protractor and karma test base code

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'app/*.js',
-      'tests/*.js'
+      'specs/**/*.js'
     ],
 
 
